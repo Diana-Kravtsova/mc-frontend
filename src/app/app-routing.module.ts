b98bc4ea18fb8534028a25400208c318381ef7ca
Diana-Kravtsova <dayana.kravtsova@gmail.com>
2021-11-20 22:09:43 +0300

    MC-16: add tab contacts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from "./pages/about/about.component";
+import { ContactsComponent } from "./pages/contacts/contacts.component";
 
 const routes: Routes = [
     { path: '', redirectTo: 'about', pathMatch: 'full' },
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: 'contacts', component: ContactsComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from "./pages/about/about.component";
+import { ContactsComponent } from "./pages/contacts/contacts.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
@@ -17,7 +18,8 @@ import { MatCardModule } from "@angular/material/card";
 @NgModule({
     declarations: [
         AppComponent,
-        AboutComponent
+        AboutComponent,
+        ContactsComponent
     ],
     imports: [
         BrowserModule,
